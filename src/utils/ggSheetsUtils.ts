0e81b125920c121990fa8521ba4a2ad432ae3a38
Souleymen Ouchane <souleymen@ouchane.fr>
2024-10-27 15:30:11 +0100

    Ajout des informations de statistiques après l'achat

--- src/utils/ggSheetsUtils.ts
+++ src/utils/ggSheetsUtils.ts
@@ -111,3 +111,69 @@ export async function addTokenToSheet(tokenAddress: string, tokenName: string, p
         throw new Error('Erreur lors de l\'ajout de la nouvelle adresse dans la feuille.');
     }
 }
+
+
+export async function addStatToSheet(whaleName: string) {
+    const sheets = google.sheets('v4');
+    const client: JWT = await getGoogleSheetsClient();
+    const spreadsheetId = process.env.GGSHEET_ID || ''; 
+    const range = 'Degen - Statistiques!A:E'; // Le même range utilisé pour la vérification
+
+    try {
+        // Récupération des lignes existantes pour déterminer où insérer la nouvelle entrée
+        const response = await sheets.spreadsheets.values.get({
+            auth: client,
+            spreadsheetId,
+            range,
+        });
+
+        const rows = response.data?.values || [];
+
+        // Définition des index des colonnes correspondantes
+        const memeCoinIndex = rows[0].indexOf('Nom du meme coin');
+        const whaleIndex = rows[0].indexOf('Whale');
+        const dateIndex = rows[0].indexOf('Date d\'achat');
+
+        if (whaleIndex === -1 || dateIndex === -1 || memeCoinIndex === -1) {
+            throw new Error('Colonnes non trouvées dans la feuille.');
+        }
+
+       // Trouver la première ligne vide dans la colonne "Clé du meme coin"
+       let firstEmptyRow = rows.length; // On commence après la dernière ligne de la feuille
+
+       for (let i = 1; i < rows.length; i++) {
+           if (!rows[i][memeCoinIndex]) { // Si la cellule est vide dans la colonne "Clé du meme coin"
+               firstEmptyRow = i + 1; // Numéro de ligne à insérer (i + 1 car les lignes sont indexées à partir de 1 dans Google Sheets)
+               break;
+           }
+       }
+       firstEmptyRow += 1; // Ajout d'une ligne supplémentaire pour insérer la nouvelle entrée
+
+       // Formater la date du jour au format "dd/MM/yyyy"
+       const today = new Date();
+       const formattedDate = `${String(today.getDate()).padStart(2, '0')}/${String(today.getMonth() + 1).padStart(2, '0')}/${today.getFullYear()}`;
+
+       // Préparation des données à insérer uniquement pour les colonnes spécifiées
+       const newRow = Array(rows[0].length).fill(''); // Crée une ligne vide de la même longueur que les autres
+       newRow[whaleIndex] = whaleName; // Insertion de l'adresse du token
+       newRow[dateIndex] = formattedDate; // Insertion de la date d'achat
+
+       // Ajout des données à la première ligne vide trouvée
+       const appendRange = `Degen - Statistiques!A${firstEmptyRow}`; // Spécifie la ligne à insérer (A)
+
+       await sheets.spreadsheets.values.update({
+           auth: client,
+           spreadsheetId,
+           range: appendRange,
+           valueInputOption: 'RAW', // Option pour insérer les valeurs telles quelles
+           requestBody: {
+               values: [newRow], // Ajout de la nouvelle ligne sous forme de tableau
+           },
+       });
+
+        console.log('Informations du token ajoutés avec succès dans l\'onglet de statistiques du Google Sheets.');
+    } catch (error) {
+        console.error('Erreur lors de l\'ajout des stats dans la feuille :', error);
+        throw new Error('Erreur lors de l\'ajout des stats dans la feuille.');
+    }
+}
